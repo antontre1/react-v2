3d74f45c56fbcea0f7f05625f1a9453b3a5debd3
Antonin Folliasson <antonin@aitio.co>
2022-08-03 17:34:37 +0200

    multiple routes and - Link -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Create from './Create'
 
 function App() {
 
@@ -8,12 +10,21 @@ function App() {
   const likes = 50
 
   return (
+  <Router>
   <div className="App">
     <Navbar />
     <div className="content">
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
     </div>
   </div>
+  </Router>
   )
 }
 
---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return (
+    <div className="create">
+      <h1>This is to create a blog !</h1>
+    </div>
+   );
+}
+
+export default Create;
