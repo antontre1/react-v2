c09d175f9b514d64a921c05f7b83b496addce42e
Antonin Folliasson <antonin@aitio.co>
2022-08-03 09:28:32 +0200

    pending message 'loadding'

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from 'react'
-import Bloglist from './Bloglist';
+import Bloglist from './Bloglist'
+
 
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null)
-
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        console.log(data)
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json()
+        })
+        .then((data) => {
+          console.log(data)
+          setIsPending(false)
+          setBlogs(data)
+        })
+    }, 1000);
+
   }, [])
 
   return (
     <div className="home">
+      { isPending && <div>Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title={ "All blogs" } />}
     </div>
    );
