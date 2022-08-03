11ca6b552e8b596485b6bc29a00b1943e627c2ff
Antonin Folliasson <antonin@aitio.co>
2022-08-03 08:58:00 +0200

    add useEffect and dependency

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Bloglist from './Bloglist';
 
 const Home = () => {
@@ -7,9 +7,10 @@ const Home = () => {
     {title: 'My First Blog', body: 'Lorem, ipsum dolor...', author: 'mario', id: 1},
     {title: 'My luigi Blog', body: 'Lorem, ipsum dolor...', author: 'luigi', id: 2},
     {title: 'My Yoknify Blog', body: 'Lorem, ipsum dolor...', author: 'yoshi', id: 3},
-    {title: 'My Yoknify Blog', body: 'Lorem, ipsum dolor...', author: 'mario', id: 4}
+    {title: 'My 2nd Yoknify Blog', body: 'Lorem, ipsum dolor...', author: 'mario', id: 4}
   ])
 
+  const [name, setName] = useState('mario')
 
   const handleDelete = (id) => {
     console.log(id)
@@ -18,9 +19,15 @@ const Home = () => {
     setBlogs(newBlogs)
   }
 
+  useEffect(() => {
+    console.log('use effect ran !')
+    console.log(name)
+  }, [name])
+
   return (
     <div className="home">
       <Bloglist blogs={blogs} title={ "All blogs" } handleDelete={ handleDelete } />
+      <button onClick={() => setName('luigi')}>update name</button>
     </div>
    );
 }
