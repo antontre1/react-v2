3d4c032b35a5934c882edbd049b06f3032577b73
Antonin Folliasson <antonin@aitio.co>
2022-08-04 07:41:40 +0200

    useEffect cleanup - how to abort a fetch in a clean way (AbortController, and some logics added in err fetch)

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,26 +7,34 @@ const useFetch = (url) => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
+
+    const abortContent = new AbortController()
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortContent.signal })
         .then(res => {
-          console.log(res)
           if(!res.ok) {
             throw Error('Could not fetch correctly the data')
           }
           return res.json()
         })
         .then((data) => {
-          console.log(data)
           setIsPending(false)
           setData(data)
           setError(null)
         })
         .catch((err) => {
-          setIsPending(false)
-          setError(err.message)
+          if (err.name === 'AbortError') {
+            console.log("fetch aborted")
+          }
+          else {
+            setIsPending(false)
+            setError(err.message)
+          }
         })
-    }, 1000);
+    }, 1000)
+
+    return () => abortContent.abort()
 
   }, [url])
 
